0465b71cd40189f4bf792bac74d8b12895372876
wahmed <waqar.ahmed@iit.it>
2022-12-31 00:22:48 +0100

    minor change

--- exp.sh
+++ exp.sh
@@ -9,3 +9,7 @@
 #
 python3 train.py --run_name LSTM_Data4_test~28 --dataset ./Data4 --model lstm --lr 0.01 --hidden_dim 16 --num_layers 2 --batch_size 32 # --wandb
 python3 test.py --weights logs/LSTM_Data4_test~28/weights_best.tar --dataset ./Data4 --model lstm --hidden_dim 16 --num_layers 2
+#
+#
+#
+# python data_analysis.py
